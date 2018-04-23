3fc92bb0e545005c150eef3b4eb562f2a2e6fe56
matadata <christian.mata@matadata.net>
2018-04-23 09:31:28 +0200

    included legend in plots 3 and 4

--- plot3.R
+++ plot3.R
@@ -35,4 +35,5 @@ plot(x=df$DateTime, y=df$Sub_metering_1, type="n", xlab="", ylab="Energy Sub Met
 lines(x=df$DateTime, y=df$Sub_metering_1, type="l", col="black")
 lines(x=df$DateTime, y=df$Sub_metering_2, type="l", col="red")
 lines(x=df$DateTime, y=df$Sub_metering_3, type="l", col="blue")
+legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.copy(png, width=480, height=480, units="px", file="./plot3.png"); dev.off()
--- plot4.R
+++ plot4.R
@@ -39,6 +39,7 @@ plot(x=df$DateTime, y=df$Sub_metering_1, type="n", xlab="", ylab="Energy Sub Met
 lines(x=df$DateTime, y=df$Sub_metering_1, type="l", col="black")
 lines(x=df$DateTime, y=df$Sub_metering_2, type="l", col="red")
 lines(x=df$DateTime, y=df$Sub_metering_3, type="l", col="blue")
+legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 #3rd plot
 plot(x=df$DateTime, y=df$Voltage, type="l", xlab="datetime", ylab="Voltage")
 #4th plot
